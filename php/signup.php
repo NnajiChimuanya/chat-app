ca04ec6765a10edf90b59c23b7d656eb99e3ee9a
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2021-09-25 00:36:25 +0400

    wrote my code for registering the user

--- php/signup.php
+++ php/signup.php
@@ -41,6 +41,36 @@
 
                            if(in_array($ext, $name_array)) {
                                echo "okay";
+
+                               $time = time();
+
+                               $location = "./images/".$time.$firstname.$lastname.$name;
+                               
+
+                               if(move_uploaded_file($temp, $location)) {
+                                   $status = "Online";
+                                   
+                                   $stmt = $conn->prepare("INSERT INTO users (firstname, lastname, email, password, image, status) VALUES (?,?,?,?,?,?)");
+                                   $stmt->bind_param("ssssss", $firstname, $lastname, $email, $password, $location, $status);
+
+                                   if($stmt->execute()){
+                                       echo "new account created";
+
+                                       $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+
+                                       if($result->num_rows > 0) {
+                                           $row = $result->fetch_assoc();
+
+                                           $id = $row["id"];
+                                           echo $id;
+                                       }
+                                       
+                                   }
+                                
+                                } else {
+                                   echo  "error in uploading image";
+                               }
+
                            } else {
                                echo "Select a valid image format";
                            }
